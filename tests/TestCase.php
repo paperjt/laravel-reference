6db59a7e5556410550042201c318d8b1e80a8035
Jamie Taniguchi <jamie.taniguchi@paper.co>
2022-04-03 16:48:32 -0700

    - Install spatie's dto and ignition, phpunit json schema assertions - Update phpunit to use separate test db

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,44 @@
 
 namespace Tests;
 
+use EnricoStahn\JsonAssert\Assert as JsonAssert;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Testing\TestResponse;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        TestResponse::macro('assertSchemaCollection', function (string $name) {
+            $data = json_decode($this->content(), false);
+
+            if (!empty($data)) {
+                $data = $data->data;
+            }
+
+            foreach ($data as $datum) {
+                JsonAssert::assertJsonMatchesSchema(
+                    $datum,
+                    base_path('tests/Schemas/' . $name)
+                );
+            }
+        });
+
+        TestResponse::macro('assertSchemaResource', function (string $name) {
+            $data = json_decode($this->content(), false);
+
+            if (!empty($data)) {
+                $data = $data->data;
+            }
+
+            JsonAssert::assertJsonMatchesSchema(
+                $data,
+                base_path('tests/Schemas/' . $name)
+            );
+        });
+    }
 }
